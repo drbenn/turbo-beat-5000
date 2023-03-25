9fbd257c0364bbcbf8aed4aac185c3265b339c7f
ratlord <bennett.daniel@gmail.com>
2023-03-25 00:11:29 -0400

    delete meter grouping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { AppState } from './shared/state/appState.state';
 import { PercussionTrackComponent } from './percussion-track/percussion-track.component';
 import { BackdropComponent } from './backdrop/backdrop.component';
 import { SliderMeterComponent } from './slider-meter/slider-meter.component';
 import { ConsoleComponent } from './console/console.component';
 import { MeterComponent } from './meter/meter.component';
-import { MeterGroupingComponent } from './meter-grouping/meter-grouping.component';
-
-import { NgxsModule } from '@ngxs/store';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { AppState } from './shared/state/appState.state';
 import { SynthMetersComponent } from './synth-meters/synth-meters.component';
 import { SynthPadComponent } from './synth-pad/synth-pad.component';
 import { WaveformSelectComponent } from './waveform-select/waveform-select.component';
@@ -26,7 +24,6 @@ import { PlayAnimationComponent } from './play-animation/play-animation.componen
     SliderMeterComponent,
     ConsoleComponent,
     MeterComponent,
-    MeterGroupingComponent,
     SynthMetersComponent,
     SynthPadComponent,
     WaveformSelectComponent,
