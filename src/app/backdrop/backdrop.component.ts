dc423914db43f8e4db9ff7918f31625e68dc0fd8
ratlord <bennett.daniel@gmail.com>
2023-03-30 23:04:15 -0400

    backdrop animations

--- src/app/backdrop/backdrop.component.ts
+++ src/app/backdrop/backdrop.component.ts
@@ -24,20 +24,31 @@ export class BackdropComponent implements OnInit{
 
 
   ngOnInit(): void {
-    for (let i = 0; i < 200; i++) {
-      const width = String(0.5 + Math.round(Math.random() * 1))
-      const height = String(0.5 + Math.round(Math.random() * 1))
+    for (let i = 0; i < 250; i++) {
+      const size = String(0.1 + Math.round(Math.random() * 1))
       const top = String(Math.round(Math.random() * 100))
       const left = String(Math.round(Math.random() * 100))
-      const scale = String(0.5 + (Math.round(Math.random()) *0.5))
+      const scale = String(0.5 + (Math.round(Math.random()) * 0.5))
       const background =  'linear-gradient(168deg, #58b580 0%, #a67be0 50%, #6624c0 100%)'
-      console.log(top);
-      console.log(left);
+      // console.log(top);
+      // console.log(left);
+      let selectedAnimation:string;
+
+
+      if (i % 1 === 0) {
+        selectedAnimation = 'part-anim-1'
+      }
+      if (i % 2 === 0) {
+        selectedAnimation = 'part-anim-2'
+      }
+      if (i % 3 === 0) {
+        selectedAnimation = 'part-anim-3'
+      }
 
 
 
 
-      this.movingParts.push(`width: 1rem; height: 1rem; background: ${background}; border: 4px solid #FFD1F37C; border-radius: 15px; bpx-shadow: 2px 2px 5px #FFFFFF; font-size: 1.5rem; color: grey; position: absolute; z-index: -5; top:${top}%; left: ${left}%; transform: scale(${scale}); animation: part-anim-1 30s infinite`)
+      this.movingParts.push(`width: ${size}rem; height: ${size}rem; background: ${background}; border: 2px solid #FFD1F37C; border-radius: 15px; bpx-shadow: 2px 2px 5px #FFFFFF; font-size: 1.5rem; color: grey; position: absolute; z-index: -5; top:${top}%; left: ${left}%; animation: ${selectedAnimation} 60s linear infinite`)
     }
   }
 
